08821f16381213c686ca4dee5302f6a7dba7ceaf
Bill <networker747@gmail.com>
2017-04-24 20:40:05 +0000

    Adds functions exercises

---
+++ hello.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Casey");
+sayHello("Ashby");
+sayHello("Paula");
+sayHello("Leona");
+sayHello("Gina");
+sayHello("Anne");
